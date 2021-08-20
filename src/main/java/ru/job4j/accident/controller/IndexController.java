51b4e304b24a5ad1ecc55d9d19e1953593b5f135
dvamedveda <justmustdie@yandex.ru>
2021-08-20 09:58:04 +0600

    Передача данных на страницу с запросом / 1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accident/controller/IndexController.java
+++ src/main/java/ru/job4j/accident/controller/IndexController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Один из бинов Spring, автоматически добавляющийся в IoC-контейнер.
  * Обслуживает подходящие запросы к веб приложению.
@@ -19,6 +22,21 @@ public class IndexController {
      */
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("users", getUserList());
         return "index";
+    }
+
+    /**
+     * Список неких пользователей для передачи на страницу.
+     * @return список пользователей.
+     */
+    private List<String> getUserList() {
+        List<String> users = new ArrayList<>();
+        users.add("Petr Arsentev");
+        users.add("Boris Savelev");
+        users.add("Ivan Ivanov");
+        users.add("Sidor Sidorov");
+        users.add("Fedor Fedorov");
+        return users;
     }
 }
